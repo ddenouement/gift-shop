cb2569d53027b4e8d375bfe62d38edef978ebd12
Mariia Malitska <37335222+MariiaMalitska@users.noreply.github.com>
2020-02-20 23:08:21 +0200

    product edit categories select added

---
+++ web/src/main/frontend/src/app/_services/category.service.ts
@@ -0,0 +1,19 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
+import {Observable} from "rxjs";
+import {Category} from "../_models/category";
+
+@Injectable({ providedIn: 'root' })
+export class CategoryService {
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Category[]> {
+    return this.http.get<Category[]>(`${environment.apiUrl}/categories`);
+  }
+
+  getById(id: number) {
+    return this.http.get<Category>(`${environment.apiUrl}/categories/${id}`);
+  }
+}
--- web/src/main/frontend/src/app/components/product-edit/product-edit.component.ts
+++ web/src/main/frontend/src/app/components/product-edit/product-edit.component.ts
@@ -5,6 +5,8 @@ import {first} from "rxjs/operators";
 import {AlertService} from "../../_services";
 import {ProductService} from "../../_services/product.service";
 import {Product} from "../../_models/product";
+import {Category} from "../../_models/category";
+import {CategoryService} from "../../_services/category.service";
 
 @Component({
   selector: 'app-product-edit',
@@ -19,11 +21,14 @@ export class ProductEditComponent implements OnInit {
   loading = false;
   submitted = false;
 
+  categoriesList: Category[];
+
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
     private route: ActivatedRoute,
     private productService: ProductService,
+    private categoryService: CategoryService,
     private alertService: AlertService) {
 
   }
@@ -45,23 +50,38 @@ export class ProductEditComponent implements OnInit {
 
     this.sub = this.route.params.subscribe(params => {
       this.loading = true;
-      this.id = +params['id'];
-      this.productService.getById(+this.id)
-        .subscribe(productData =>{
-          this.loading = false;
-          this.current_product = productData;
 
-          this.editForm.controls["productId"].setValue(productData.productId);
-          this.editForm.controls["productName"].setValue(productData.productName);
-          this.editForm.controls["description"].setValue(productData.description);
-          this.editForm.controls["price"].setValue(productData.price);
-          this.editForm.controls["photo"].setValue(productData.photo);
-          this.editForm.controls["isAvailable"].setValue(productData.isAvailable);
+      this.categoryService.getAll().subscribe(data =>{
+          this.loading = false;
+          this.categoriesList = data;
+
+          this.id = +params['id'];
+
+          this.productService.getById(+this.id)
+            .subscribe(productData =>{
+                this.loading = false;
+                this.current_product = productData;
+
+                this.editForm.controls["productId"].setValue(productData.productId);
+                this.editForm.controls["productName"].setValue(productData.productName);
+                this.editForm.controls["description"].setValue(productData.description);
+                this.editForm.controls["price"].setValue(productData.price);
+                this.editForm.controls["photo"].setValue(productData.photo);
+                this.editForm.controls["isAvailable"].setValue(productData.isAvailable);
+                this.editForm.controls["categories"].setValue(productData.categories);
+              },
+              error => {
+                this.alertService.error(error);
+                this.loading = false;
+              });
+
+
         },
         error => {
           this.alertService.error(error);
           this.loading = false;
         });
+
     });
   }
 
